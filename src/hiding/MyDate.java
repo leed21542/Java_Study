f98983e79478c5ed73b060e9665d47642018898a
leed21542 <leed21542@naver.com>
2021-05-04 15:37:44 +0900

    commit

--- src/hiding/MyDate.java
+++ src/hiding/MyDate.java
@@ -2,7 +2,17 @@ package hiding;
 
 public class MyDate { //정보 은닉
 
-    int day;
-    int month;
-    int year;
+    private int day;
+    private int month;
+    private int year;
+
+    public void setDate(int year, int month, int day){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public void showDate(){
+        System.out.println("오늘은 "+year+"년 "+month + "월 "+day+"일 입니다.");
+    }
 }
---
+++ src/hiding/MyDateTest.java
@@ -0,0 +1,11 @@
+package hiding;
+
+public class MyDateTest {
+    public static void main(String[] args) {
+        MyDate date = new MyDate();
+
+        date.setDate(2021,5,4);
+
+        date.showDate();
+    }
+}
